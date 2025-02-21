1e0c723ef367ea5452241bb87c507713e30faddd
Douaa <douaachemnane@gmail.com>
2025-02-21 10:30:12 +0100

    try to resolve deployment error

--- app/page.js
+++ app/page.js
@@ -9,23 +9,18 @@ import Skills from "./components/homepage/skills";
 
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
-
   const data = await res.json();
-
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
+  return data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+}
 
 export default async function Home() {
   const blogs = await getData();
 
   return (
-    <div suppressHydrationWarning >
+    <main>
       <HeroSection />
       <AboutSection />
       <Experience />
@@ -33,6 +28,6 @@ export default async function Home() {
       <Projects />
       <Blog blogs={blogs} />
       <ContactSection />
-    </div>
+    </main>
   )
-};
+}
